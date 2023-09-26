0d93203343a30446424a7ca98bf03cd457a1eda6
Philipp Mulfinger <info@phil-splash.de>
2023-09-26 20:33:53 +0200

    getSingleUser Route created

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,9 +2,27 @@ import pool from "../db/server.js";
 
 export const getAllUsers = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
+    const result = await pool.query("SELECT * FROM users");
     res.json(result.rows);
   } catch (err) {
-    res.status(500).json({ message: 'something is not ok' });
+    res.status(500).json({ message: "something is not ok" });
   }
 };
+
+export const getSingleUser = async (req, res) => {
+  try {
+    const index = req.params.id;
+    const result = await pool.query(`SELECT * FROM users WHERE id=${index}`);
+    res.json(result.rows);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "something is not working in getSingleUser" });
+  }
+};
+
+export const createNewUser = (req, res) => {
+   
+
+
+}
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getAllUsers } from "../controllers/orders.js";
+import {
+  getAllUsers,
+  getSingleUser,
+  createNewUser,
+} from "../controllers/orders.js";
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", getAllUsers);
+usersRouter.get("/:id", getSingleUser);
+usersRouter.post("/", createNewUser);
 
 export default usersRouter;
